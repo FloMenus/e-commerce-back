8e07cfe25fc0509c51cf9c5127615ef02dbc8eac
FloMenus <florent.mns@gmail.com>
2022-09-07 17:17:06 +0200

    fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const categoriesRoutes = require("./routes/categories");
 const productsRoutes = require("./routes/products");
 const ordersRoutes = require("./routes/orders");
+const morgan = require("morgan");
 
 app.use(express.json());
 app.use(cors());
