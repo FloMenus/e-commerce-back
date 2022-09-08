0581cf1aeca6302b16168fae9e8cc4d5f2f8f798
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-09-08 14:31:29 +0200

    change on database

--- models/index.js
+++ models/index.js
@@ -1,44 +1,45 @@
-require ("dotenv").config();
-const { Sequelize } = require('sequelize');
+require("dotenv").config()
+const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
     host: process.env.HOST,
-    dialect: 'mysql',
-    logging: false
-});
+    dialect: "mysql",
+    logging: false,
+  }
+)
 
 const connectDb = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established.');
-    }
-    catch (error) {
-        console.error('Unable to connect to the database:' + error);
-    }
-};
+  try {
+    await sequelize.authenticate()
+    console.log("Connection has been established.")
+  } catch (error) {
+    console.error("Unable to connect to the database:" + error)
+  }
+}
 
-connectDb();
+connectDb()
 
-const Product = require('./product')(sequelize);
-const Order = require('./order')(sequelize);
-const Category = require('./category')(sequelize);
+const Product = require("./product")(sequelize)
+const Order = require("./order")(sequelize)
+const Category = require("./category")(sequelize)
 
-Category.hasMany(Product);
-Product.belongsToMany(Category, { through: 'product_category' });
+Category.belongsToMany(Product, { through: "product_category" })
+Product.belongsToMany(Category, { through: "product_category" })
 
-Order.hasMany(Product);
-Product.belongsToMany(Order, { through: 'order_product' });
+Order.belongsToMany(Product, { through: "order_product" })
+Product.belongsToMany(Order, { through: "order_product" })
 
-
-
-
-sequelize.sync({ alter: true });
+sequelize.sync({ alter: true })
 
 const db = {
-    sequelize,
-    Product,
-    Order,
-    Category,
-};
+  sequelize,
+  Product,
+  Order,
+  Category,
+}
 
 module.exports = db
